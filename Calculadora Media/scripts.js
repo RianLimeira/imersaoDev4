44accbf13afcc244340cf1cadf3f1e948f8d6fc7
RianLimeira <68974304+RianLimeira@users.noreply.github.com>
2022-03-09 14:52:25 -0300

    Update scripts.js
    Adicionando alerta para inserir nome e notas, e mostrar na tela as respectivas informações

--- Calculadora Media/scripts.js
+++ Calculadora Media/scripts.js
@@ -1,9 +1,12 @@
-var nome = "Rian"
-var nota1 = 9
-var nota2 = 7
-var nota3 = 8
-var nota4 = 4
-var media = (nota1 + nota2 + nota3 + nota4) / 4
+var nome = prompt("Qual seu nome?")
+var nota1 = prompt("Qual sua primeira nota?")
+var nota2 = prompt("Qual sua segunda nota?")
+var nota3 = prompt("Qual sua terceira nota?")
+var nota4 = prompt("Qual sua quarta nota?")
+
+var media = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3) + parseFloat(nota4)) / 4
+
+
 
 console.log("Bem vindo " + nome)
 console.log("Suas notas foram " + nota1 + ", " + nota2 + ", " + nota3 + ", " + nota4)
@@ -11,10 +14,12 @@ console.log("media = " + media)
 
 if (media >= 7) {
     console.log("aprovado")
+    document.writeln("Notas: " + nota1 + ", " + nota2 + ", " + nota3 + ", " + nota4, '<br/>')
     document.write('Aprovado com ' + media)
     alert('OK')
 } else {
     console.log("reprovado")
+    document.writeln("Notas: " + nota1 + ", " + nota2 + ", " + nota3 + ", " + nota4, '<br/>')
+    document.writeln("Média: " + media)
     alert("Not OK")
 }
-//console.log(" e media: "+ ((nota1+ nota2+ nota3+nota4)/4))
